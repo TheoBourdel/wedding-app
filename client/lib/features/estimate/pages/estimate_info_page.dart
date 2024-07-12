367a9ea011a86ea6be30b4889f33439670dac7c9
Amilcar-afk <fernandesamilcar28@gmail.com>
2024-07-12 23:01:12 +0200

    add payment ok front

--- client/lib/features/estimate/pages/estimate_info_page.dart
+++ client/lib/features/estimate/pages/estimate_info_page.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:client/features/auth/bloc/auth_bloc.dart';
 import 'package:client/features/auth/bloc/auth_state.dart';
 import 'package:client/features/estimate/bloc/estimate_bloc.dart';
@@ -6,12 +7,89 @@ import 'package:client/features/estimate/widgets/estimate_bottom_modal_form.dart
 import 'package:client/model/estimate.dart';
 import 'package:client/shared/widget/button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_braintree/flutter_braintree.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../repository/estimate_repository.dart';
 
 class EstimateInfoPage extends StatelessWidget {
   final Estimate estimate;
   const EstimateInfoPage({super.key, required this.estimate});
+
+  Future<void> _startBraintreePayment(BuildContext context, userId) async {
+    print("Starting Braintree payment...");
+    var request = BraintreeDropInRequest(
+      tokenizationKey: 'sandbox_jybm8wfr_qsn76kgd9qtrkyv5',
+      collectDeviceData: true,
+      requestThreeDSecureVerification: true,
+      googlePaymentRequest: BraintreeGooglePaymentRequest(
+        totalPrice: estimate.price.toString(),
+        currencyCode: 'EUR',
+        billingAddressRequired: false,
+      ),
+      paypalRequest: BraintreePayPalRequest(
+        amount: estimate.price.toString(),
+        displayName: 'Example company',
+      ),
+      cardEnabled: true,
+    );
+
+    BraintreeDropInResult? result = await BraintreeDropIn.start(request);
+    if (result != null) {
+      print('Nonce received: ${result.paymentMethodNonce.nonce}');
+      await _sendNonceToServer(context, result.paymentMethodNonce.nonce, userId);
+    } else {
+      print('Payment was cancelled or failed.');
+    }
+  }
+
+  Future<void> _sendNonceToServer(BuildContext context, String nonce, userId) async {
+    try {
+      print('Sending nonce to server: $nonce');
+      await EstimateRepository.payEstimate(estimate.id, nonce);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Payment Successful')));
+      context.read<EstimateBloc>().add(
+          EstimateUpdateEvent(
+            estimate: estimate.copyWith(
+              status: 'accepted',
+              price: estimate.price,
+              content: estimate.content,
+            ),
+            userId: userId,
+          )
+      );
+
+      Navigator.pop(context);
+    } catch (error) {
+      print('Payment failed: $error');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Payment Failed: $error')));
+    }
+  }
+
+  /*Future<void> _startCreditCardPayment(BuildContext context) async {
+    print("Starting Braintree credit card payment...");
+    var request = BraintreeCreditCardRequest(
+      cardNumber: '4111111111111111',
+      expirationMonth: '12',
+      expirationYear: '2025',
+      cvv: '123',
+    );
+
+    BraintreePaymentMethodNonce? result = await Braintree.tokenizeCreditCard(
+      'sandbox_jybm8wfr_qsn76kgd9qtrkyv5',
+      request,
+    );
+
+    if (result != null) {
+      print('Nonce received: ${result.nonce}');
+      await _sendNonceToServer(context, result.nonce);
+    } else {
+      print('Payment was cancelled or failed.');
+    }
+  }*/
+
   @override
   Widget build(BuildContext context) {
     Widget? buttons() {
@@ -29,9 +107,9 @@ class EstimateInfoPage extends StatelessWidget {
                   showModalBottomSheet(
                     context: context,
                     builder: (context) => EstimateBottomModalForm(
-                      estimate: estimate,
-                      userId: userId!,
-                      title: 'Creer'
+                        estimate: estimate,
+                        userId: userId!,
+                        title: 'Creer'
                     ),
                   );
                 },
@@ -42,14 +120,14 @@ class EstimateInfoPage extends StatelessWidget {
                 isOutlined: true,
                 onPressed: () {
                   context.read<EstimateBloc>().add(
-                    EstimateUpdateEvent(
-                      estimate: estimate.copyWith(
-                        status: 'canceled',
-                        price: estimate.price,
-                        content: estimate.content,
-                      ),
-                      userId: userId!,
-                    )
+                      EstimateUpdateEvent(
+                        estimate: estimate.copyWith(
+                          status: 'canceled',
+                          price: estimate.price,
+                          content: estimate.content,
+                        ),
+                        userId: userId!,
+                      )
                   );
                   Navigator.pop(context);
                 },
@@ -60,34 +138,34 @@ class EstimateInfoPage extends StatelessWidget {
 
         if (estimate.status == 'pending') {
           return Button(
-            text: "Modifier",
-            isOutlined: true,
-            onPressed: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (context) => EstimateBottomModalForm(
-                  estimate: estimate,
-                  userId: userId!,
-                  title: 'Modifier'
-                ),
-              );
-            }
+              text: "Modifier",
+              isOutlined: true,
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) => EstimateBottomModalForm(
+                      estimate: estimate,
+                      userId: userId!,
+                      title: 'Modifier'
+                  ),
+                );
+              }
           );
         }
       } else if (userRole == 'marry') {
         if (estimate.status == 'requesting') {
           return Button(
-            text: "Annuler",
-            isOutlined: true,
-            onPressed: () {
-              context.read<EstimateBloc>().add(
-                EstimateDeleteEvent(
-                  estimateId: estimate.id,
-                  userId: userId!,
-                )
-              );
-              Navigator.pop(context);
-            }
+              text: "Annuler",
+              isOutlined: true,
+              onPressed: () {
+                context.read<EstimateBloc>().add(
+                    EstimateDeleteEvent(
+                      estimateId: estimate.id,
+                      userId: userId!,
+                    )
+                );
+                Navigator.pop(context);
+              }
           );
         }
 
@@ -95,20 +173,8 @@ class EstimateInfoPage extends StatelessWidget {
           return Column(
             children: [
               Button(
-                text: "Accepter",
-                onPressed: () {
-                  context.read<EstimateBloc>().add(
-                    EstimateUpdateEvent(
-                      estimate: estimate.copyWith(
-                        status: 'accepted',
-                        price: estimate.price,
-                        content: estimate.content,
-                      ),
-                      userId: userId!,
-                    )
-                  );
-                  Navigator.pop(context);
-                },
+                text: "Confirmer et payer",
+                onPressed: () => _startBraintreePayment(context, userId),
               ),
               const SizedBox(height: 10),
               Button(
@@ -116,34 +182,35 @@ class EstimateInfoPage extends StatelessWidget {
                 isOutlined: true,
                 onPressed: () {
                   context.read<EstimateBloc>().add(
-                    EstimateUpdateEvent(
-                      estimate: estimate.copyWith(
-                        status: 'canceled',
-                        price: estimate.price,
-                        content: estimate.content,
-                      ),
-                      userId: userId!,
-                    )
+                      EstimateUpdateEvent(
+                        estimate: estimate.copyWith(
+                          status: 'canceled',
+                          price: estimate.price,
+                          content: estimate.content,
+                        ),
+                        userId: userId!,
+                      )
                   );
                   Navigator.pop(context);
                 },
               ),
+              SizedBox(height: 20),
             ],
           );
         }
       }
       return null;
     }
-    
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Information du devis"),
         actions: [
           if (estimate.status != 'requesting')
             IconButton(
-            icon: const Icon(Iconsax.document_download),
-            onPressed: () {}
-          )
+                icon: const Icon(Iconsax.document_download),
+                onPressed: () {}
+            )
         ],
       ),
       backgroundColor: Colors.grey[100],
